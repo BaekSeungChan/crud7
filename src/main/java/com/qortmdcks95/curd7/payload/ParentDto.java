5f16fefb956140af6f55cc5a5ba74a82346e3c9f
baekseungchan1117 <bsc7386@gmail.com>
2023-11-14 16:34:01 +0900

    valid, default

--- src/main/java/com/qortmdcks95/curd7/payload/ParentDto.java
+++ src/main/java/com/qortmdcks95/curd7/payload/ParentDto.java
@@ -1,12 +1,16 @@
 package com.qortmdcks95.curd7.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ParentDto {
     private Long id;
 
+    @NotEmpty
+    @Size(min = 2, message = "you have least 10 characters")
     private String name;
 
-    private Integer age;
+    private Integer age = 10;
 }
